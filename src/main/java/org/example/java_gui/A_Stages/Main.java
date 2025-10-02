69f7f4ef87dfcf81b252fedf9ee678c4653f55cb
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-10-02 11:39:15 -0300

    A stages

--- src/main/java/org/example/java_gui/A_Stages/Main.java
+++ src/main/java/org/example/java_gui/A_Stages/Main.java
@@ -1,7 +1,36 @@
 package org.example.java_gui.A_Stages;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.GREEN);
+
+        Image icon = new Image("settings.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Stage Demo Program");
+        stage.setWidth(420);
+        stage.setHeight(420);
+        stage.setResizable(false);
+//        stage.setX(0);
+//        stage.setY(0);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("Press (q) to escape");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
